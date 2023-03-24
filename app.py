90d59966416e0b25f253baf1265766f788446c7b
chandrababuchavuturi <39554618+chandrababuchavuturi@users.noreply.github.com>
2023-03-24 10:10:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask
-app = Flask(__name__)
 
+
+@app.route('/')
 def home():
     return "<h1 style='text-align: center;'>Welcome to my Cloud DevOps Engineer Capstone Project!</h1>"
 
